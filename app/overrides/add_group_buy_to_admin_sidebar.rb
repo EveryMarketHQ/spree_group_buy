bf9a5c66ac501abfdf4ba159b2bcc20556dde1fb
Josh <joshyanworks@gmail.com>
2020-11-30 12:49:00 -0600

    upgrade

--- app/overrides/add_group_buy_to_admin_sidebar.rb
+++ app/overrides/add_group_buy_to_admin_sidebar.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_main_menu',
   name: 'group_buy_to_admin_sidebar',
-  insert_top: 'nav',
+  replace: '#sidebarOrder',
   partial: 'spree/admin/shared/group_buy_sidebar_menu'
 )
 
